797b2844561414ff184bc9091f114ac0a27b0897
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-19 11:13:42 +0200

    add 'contains correct header' test case

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Fundamentals test', () => {
+  it('Contains correct header text', () => {
+    cy.visit('/fundamentals')
+    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+    cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
   })
 })
