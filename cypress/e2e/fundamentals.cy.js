9b15567dfb251f5d6ece46b15695b9567fdb4a72
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-20 11:29:05 +0200

    add beforEach

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,12 +1,14 @@
 describe('Fundamentals test', () => {
-  it('Contains correct header text', () => {
+  beforeEach(() => {
     cy.visit('/fundamentals')
+  })
+
+  it('Contains correct header text', () => {
     cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
     cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
   })
 
   it('Accordion works correctly', () => {
-    cy.visit('/fundamentals')
     cy.contains(/Your tests will exist in a describe block./i).should('not.be.visible')
     cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
     cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
