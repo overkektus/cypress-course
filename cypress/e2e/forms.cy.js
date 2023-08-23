d6ae54a4fbc5340ec89a200c62831dacfb7937a3
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-23 12:24:20 +0200

    update 'test subscribe form' test case

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -1,13 +1,33 @@
 describe('form tests', () => {
+  const validEmail = 'egor.piskunou@gmail.com'
+  const invalidEmail = 'egor.piskunou@gmail.io'
+
   beforeEach(() => {
     cy.visit('/forms')
   })
 
   it('Test subscribe form', () => {
     cy.contains(/testing forms/i)
-    cy.getDataTest('subscribe-form').find('input').type('egor.piskunou@gmail.com')
-    cy.contains(/Successfully subbed: egor.piskunou@gmail.com!/i).should('not.exist')
+    cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+    cy.get('@subscribe-input').type(validEmail)
+    cy.contains(new RegExp(`Successfully subbed: ${validEmail}!`, 'i')).should('not.exist')
     cy.getDataTest('subscribe-button').click()
-    cy.contains(/Successfully subbed: egor.piskunou@gmail.com!/i).should('exist')
+    cy.contains(new RegExp(`Successfully subbed: ${validEmail}!`, 'i')).should('exist');
+    cy.wait(3000)
+    cy.contains(/Successfully subbed: !/i).should('not.exist')
+
+    cy.get('@subscribe-input').type(invalidEmail)
+    cy.contains(new RegExp(`invalid email: ${invalidEmail}`, 'i')).should('not.exist')
+    cy.getDataTest('subscribe-button').click()
+    cy.contains(new RegExp(`invalid email: ${invalidEmail}`, 'i')).should('exist')
+    cy.wait(3000)
+    cy.contains(new RegExp(`invalid email: ${invalidEmail}`, 'i')).should('not.exist')
+
+    cy.contains(/fail!/i).should('not.exist')
+    cy.get('@subscribe-input')
+    cy.getDataTest('subscribe-button').click()
+    cy.contains(/fail!/i).should('exist')
+    cy.wait(3000)
+    cy.contains(/fail!/i).should('not.exist')
   })
 })
